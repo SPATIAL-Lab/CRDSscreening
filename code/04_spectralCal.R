593741b5d836b127e4c5c8a96e9b0a11d853563a
Gabriel Bowen <gabe.bowen@utah.edu>
2024-12-06 07:09:31 -0700

    Update calibration

--- code/04_spectralCal.R
+++ code/04_spectralCal.R
@@ -1,6 +1,7 @@
 library(MASS)
 
-# Read data
+# Prep ----
+## Read data
 pirms = read.csv("out/pirms.csv")
 sirms = read.csv("out/sirms.csv")
 
@@ -9,8 +10,9 @@ pirms = pirms[, names(pirms) != "Species"]
 names(sirms) = names(pirms)
 ad = rbind(pirms, sirms)
 
+# Optimize model ----
 ## Tolerance for 'good' samples
-ad$Good = ad$d18O - ad$d18O.irms < 1.3 & ad$d18O - ad$d18O.irms > -1
+ad$Good = ad$d18O - ad$d18O.irms < 1.3 & ad$d18O - ad$d18O.irms > -1.3
 
 ## LDA cross validation
 ld.cv = lda(Good ~ BaseShift + SlopeShift + Residuals + BaseCurve + CH4, 
@@ -24,7 +26,26 @@ points(ad$d18O.irms[ad$Good], ad$d18O[ad$Good], pch = 21, bg = "white",
 points(ad$d18O.irms[ld.cv$class], ad$d18O[ld.cv$class], 
        pch = 21, bg = "blue")
 
-## Fit full model
+# Test model ----
+s1 = s2 = numeric()
+
+for(i in 1:100){
+  ind = sample(1:nrow(ad), 50)
+  tr = ad[ind,]
+  ts = ad[-ind,]
+  ld = lda(Good ~ BaseShift + SlopeShift + Residuals + BaseCurve + CH4,
+           data = tr, prior = c(0.6, 0.4))
+  g = as.logical(predict(ld, ts)$class)
+  
+  s1 = c(s1, sd(ts$d18O - ts$d18O.irms))
+  s2 = c(s2, sd(ts$d18O[g] - ts$d18O.irms[g])) 
+}
+
+mean(s1)
+mean(s2)
+
+# Full model ----
+## Fit model
 ld = lda(Good ~ BaseShift + SlopeShift + Residuals + BaseCurve + CH4, 
          data = ad, prior = c(0.6, 0.4))
 ld
